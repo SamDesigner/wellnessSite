13f4b771031834c6fdf93ac0194960ef9daa9290
MacBookPro <macbookpro@MacBookPros-MacBook-Pro.local>
2024-05-13 20:48:03 +0100

    my first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var action = document.querySelector(".action");
+var mobileMenu = document.querySelector(".mobile_menu");
+var hamburger = document.querySelector('.hamburger');
+var cancel = document.querySelector('.cancel');
+action.addEventListener("click" , ()=> {
+    mobileMenu.classList.toggle('reset');
+    if(mobileMenu.classList.contains('reset')){
+        hamburger.style.display= 'none';
+        cancel.style.display = 'block'
+    }else{
+        hamburger.style.display= 'block';
+        cancel.style.display = 'none'
+    }
+})
